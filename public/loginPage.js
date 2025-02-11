85a39341cc502a974cb3a45ea68a6c6868643cc5
Neto test <nikolajeliseev834@gmail.com>
2025-02-11 13:17:44 +0300

    UserForm уже есть в UserForm.js, к нему нужно просто дописать 2 функции, исправляю код

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,36 +1,37 @@
-"use strict"
+"use strict";
 
-class UserForm {
-  constructor(data) {
-    this.userMap = new Map(data.map);
-  }
-
-  loginFormCallback(data) {
-    ApiConnector.login(data, (response) => {
-      if (response.success) {
-        location.reload();
-      } else {
-        console.warn(`Пользователь с логином ${data.login} и указанным паролем не найден.`);
-        alert('Ошибка входа: Неверный логин или пароль.');
-      }
-    });
-  }
-}
-
-const data = {
-  map: [
-    ["oleg@demo.ru", "demo"],
-    ["ivan@demo.ru", "demo"],
-    ["petr@demo.ru", "demo"],
-    ["galina@demo.ru", "demo"],
-    ["vladimir@demo.ru", "demo"],
-  ],
-};
-
+// class UserForm {
+// constructor(data) {
+//   this.userMap = new Map(data.map);
+// }
 const userForm = new UserForm(data);
 
 userForm.loginFormCallback = (data) => {
-  ApiConnector.login(data, callback);
+  ApiConnector.login(data, (response) => {
+    if (response.success) {
+      location.reload();
+    } else {
+      console.warn(
+        `Пользователь с логином ${data.login} и указанным паролем не найден`
+      );
+      alert("Ошибка входа: Неверный логин или пароль");
+    }
+  });
 };
+// }
 
-console.log(userForm.userMap);
+// const data = {
+//   map: [
+//     ["oleg@demo.ru", "demo"],
+//     ["ivan@demo.ru", "demo"],
+//     ["petr@demo.ru", "demo"],
+//     ["galina@demo.ru", "demo"],
+//     ["vladimir@demo.ru", "demo"],
+//   ],
+// };
+
+// userForm.loginFormCallback = (data) => {
+//   ApiConnector.login(data, callback);
+// };
+
+// console.log(userForm.userMap);
