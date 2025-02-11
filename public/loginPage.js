cee4c08f6abac8bb7024d3eb7919c33f129fe640
Neto test <nikolajeliseev834@gmail.com>
2025-02-11 13:36:54 +0300

    Добавил 2 функцию - registerFormCallback

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,37 +1,29 @@
 "use strict";
 
-// class UserForm {
-// constructor(data) {
-//   this.userMap = new Map(data.map);
-// }
-const userForm = new UserForm(data);
+const userForm = new UserForm();
 
 userForm.loginFormCallback = (data) => {
   ApiConnector.login(data, (response) => {
     if (response.success) {
       location.reload();
     } else {
-      console.warn(
+      console.error(
         `Пользователь с логином ${data.login} и указанным паролем не найден`
       );
       alert("Ошибка входа: Неверный логин или пароль");
     }
   });
-};
-// }
+}
 
-// const data = {
-//   map: [
-//     ["oleg@demo.ru", "demo"],
-//     ["ivan@demo.ru", "demo"],
-//     ["petr@demo.ru", "demo"],
-//     ["galina@demo.ru", "demo"],
-//     ["vladimir@demo.ru", "demo"],
-//   ],
-// };
-
-// userForm.loginFormCallback = (data) => {
-//   ApiConnector.login(data, callback);
-// };
-
-// console.log(userForm.userMap);
+userForm.registerFormCallback = (data) => {
+  ApiConnector.register(data, (response) => {
+    if (response.success) {
+      location.reload();
+    } else {
+      console.errer(
+        `Пользователь с логином ${data.login} и указанным паролем не найден`
+      );
+      alert("Ошибка входа: Неверный логин или пароль");
+    }
+  });
+}
